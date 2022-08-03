3c7a378038f70ad8da3825cfeb38377f81ab164a
stanislav <rebel1866@yandex.ru>
2022-08-03 16:36:53 +0300

    init

---
+++ src/main/java/com/kofax/textviewer/TextviewerApplication.java
@@ -0,0 +1,13 @@
+package com.kofax.textviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kofax/textviewer/TextviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kofax.textviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
